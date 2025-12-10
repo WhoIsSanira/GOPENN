fd9f8af5d944e47306bf7172afd8495afbbfa526
WhoIsSanira <turganova.sanira.01@gmail.com>
2025-12-10 15:59:21 +0500

    Fix and refactor NN training pipeline

--- code/v1.py
+++ code/v1.py
@@ -1,23 +1,56 @@
 import numpy as np
 import tensorflow as tf
 import keras
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 
-dataset = []
-reaction_params = []
-gops = []
+def prepare_data(dataset) -> tuple:
+    xs = []
+    ys = []
+    scaler_x = StandardScaler()
+    scaler_y = StandardScaler()
 
-for i in dataset:
-    reaction_params.append(dataset[i][0])
-    gops.append(dataset[i][1])
+    for i in dataset:
+        xs.append(dataset[i][0])
+        ys.append(dataset[i][1])
+
+    xs_scaled = scaler_x.fit_transform(xs)
+    ys_scaled = scaler_y.fit_transform(ys)
+
+    xs_train, xs_test, ys_train, ys_test = train_test_split(xs_scaled, ys_scaled, test_size=0.2, train_size=0.8)
+    prepared_data = xs_train, xs_test, ys_train, ys_test
+
+    return prepared_data
+
+
+def build_model():
+    model = keras.Sequential([
+        keras.layers.Input(shape=(5,)),
+        keras.layers.Dense(100, activation='relu'),
+        keras.layers.Dense(100, activation='relu'),
+        keras.layers.Dense(100, activation='relu'),
+        keras.layers.Dense(100, activation='relu'),
+        keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer=keras.optimizers.Adam(), loss='mse', metrics=['mae'])
+
+    return model
+
+if __name__ == '__main__':
+    dataset = []
+    reaction_params_train, reaction_params_test, gops_train, gops_test = prepare_data(dataset)
+    model = build_model()
+    EPOCHS = 100
+    checkpoint_filepath = ''
+
+    callbacks = [keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True, start_from_epoch=10), 
+                 keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath, monitor='loss', save_best_only=True)]
     
-model = keras.Sequential([
-    keras.layers.InputLayer(5),
-    keras.layers.Dense(100, activation='tanh'),
-    keras.layers.Dense(100, activation='tanh'),
-    keras.layers.Dense(100, activation='tanh'),
-    keras.layers.Dense(100, activation='tanh'),
-    keras.layers.Dense(10)
-])
+    model.fit(x=reaction_params_train, y=gops_train, epochs=EPOCHS, callbacks=callbacks)
+    model.save('v1.h5')
 
-model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.mean_squared_error())
+    loss, error = model.evaluate(reaction_params_test, gops_test, verbose=2)
+    print("Loss = ", loss)
+    print("Error = ", error)
